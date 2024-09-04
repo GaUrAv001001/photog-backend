c5e425a4b84aa4365df58d28a2e35094b2ce8e18
GaUrAv001001 <gauravhiwarale1448@gmail.com>
2024-09-04 16:35:55 +0530

    add user and image model

---
+++ src/models/image.mode.js
@@ -0,0 +1,42 @@
+import mongoose, { Schema } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const imageSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
+    },
+
+    url: {
+      type: String, // url from cloudinary
+      required: true,
+    },
+
+    description: {
+      type: String,
+      require: true,
+    },
+
+    isPublic: {
+      type: Boolean,
+      default: false,
+    },
+
+    uploadedBy: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+
+imageSchema.plugin(mongooseAggregatePaginate)
+
+export const Image = mongoose.model("Image", imageSchema);
